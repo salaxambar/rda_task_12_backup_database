89499fd4069ca0ca5e746f66287c3ccb9fb04f0b
Marko Panchak <marko.panchak1@gmail.com>
2024-06-27 15:26:22 +0300

    Solution_fix_1

--- task.sh
+++ task.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 mysqldump -u $DB_USER -p"$DB_PASSWORD" ShopDB --no-create-db --result-file=ShopDB_backup.sql
-mysqldump -u $DB_USER -p"$DB_PASSWORD" ShopDBReserve < ShopDB_backup.sql
+mysql -u $DB_USER -p"$DB_PASSWORD" ShopDBReserve < ShopDB_backup.sql
 mysqldump -u $DB_USER -p"$DB_PASSWORD" ShopDB --no-create-db --no-create-info --result-file=ShopDB_backup_data.sql
-mysqldump -u $DB_USER -p"$DB_PASSWORD" ShopDBDevelopment < ShopDB_backup_data.sql
+mysql -u $DB_USER -p"$DB_PASSWORD" ShopDBDevelopment < ShopDB_backup_data.sql
 
